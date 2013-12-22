09f7bac0f6ef745f91ac2f4e05478df85d56e01a
Kent Fredric <kentfredric@gmail.com>
2013-12-23 01:32:49 +1300

    Add bulk corelist translator

---
+++ util/transpose_corelist_all.sh
@@ -0,0 +1,18 @@
+#!bash
+
+
+perl util/transpose_corelist.pl 5.8.8  NWCLARK/perl-5.8.8.tar.gz
+perl util/transpose_corelist.pl 5.10.1 DAPM/perl-5.10.1.tar.gz
+perl util/transpose_corelist.pl 5.12.3 RJBS/perl-5.12.3.tar.gz
+perl util/transpose_corelist.pl 5.12.4 LBROCARD/perl-5.12.4.tar.gz
+perl util/transpose_corelist.pl 5.12.5 DOM/perl-5.12.5.tar.gz
+perl util/transpose_corelist.pl 5.14.1 JESSE/perl-5.14.1.tar.gz
+perl util/transpose_corelist.pl 5.14.2 FLORA/perl-5.14.2.tar.gz
+perl util/transpose_corelist.pl 5.16.0 RJBS/perl-5.16.0.tar.gz
+perl util/transpose_corelist.pl 5.16.1 RJBS/perl-5.16.1.tar.gz
+perl util/transpose_corelist.pl 5.16.2 RJBS/perl-5.16.2.tar.gz
+perl util/transpose_corelist.pl 5.16.3 RJBS/perl-5.16.3.tar.gz
+perl util/transpose_corelist.pl 5.18.1 RJBS/perl-5.18.1.tar.gz
+
+
+
